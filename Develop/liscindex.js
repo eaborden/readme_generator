0ccb1f3f6f745aa2dc1bc593e94094c77fe97e83
Edward Borden <arroll.borden@gmail.com>
2020-10-05 17:11:08 -0400

    proposed javascript file for to populate license badge & license information

---
+++ Develop/liscindex.js
@@ -0,0 +1,23 @@
+const = "MIT"
+
+const = "MITliscText"
+
+A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+const = "Apache"
+
+const = "ApacheliscText"
+
+A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+const = "GPLv3"
+
+const = "GPLv3LicenseText"
+
+Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
