07a8e49bffd80727b04fe5a9526fd1fcaff949ca
Edward Borden <arroll.borden@gmail.com>
2020-10-05 14:07:35 -0400

    markdown file with intial formatting

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,39 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.title}
+
+  # Description  
+  ${data.description}
+
+  # Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#test)
+  * [Questions](#questions)
+  
+  # Installation
+
+  ${data.installation}
+ 
+  # Usage
+  ${data.usage}
+  
+  # License
+  ${data.license}
+  
+  # Contributing
+  ${data.contributing}
+  
+  # Tests
+  ${data.test}
+  
+  # Question
+  ${data.githubRepo}
+  ${data.emailAddress}
 
 `;
 }
